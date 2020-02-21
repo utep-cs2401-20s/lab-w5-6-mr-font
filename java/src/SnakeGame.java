c9ef62f285ef31a9ebe545c59b96e9569c894967
mr-font <mrfont@miners.utep.edu>
2020-02-21 00:50:10 -0700

    2/21/2020 Modified findTailExhaustive method

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -24,17 +24,31 @@ public class SnakeGame {
             for (int j = 0; j <game[0].length; j++){
 
                 if(i == headPosition[0] && j == headPosition[1]){
-                    if(){}
+
+                    length++;
                     counter++;
-                    continue;
+                    //continue;
                 }
 
                 if(game[i][j] == true){
+                  length++;
+                  counter++;
+                  if(game[i+1][j] == true && !game[i-1][j] && !game[i][j+1] && !game[i][j-1] || game[i][j+1] == true && !game[i+1][j] && !game[i-1][j] && !game[i][j-1] || game[i][j-1] == true && !game[i][j+1] && !game[i+1][j] && !game[i-1][j] || game[i-1][j] == true && !game[i+1][j] && !game[i][j+1] && !game[i][j-1] ){
+                      tailArray[0] = i;
+                      tailArray[1] = j;
+                  }
+                  //if(i > 0 && i < game.length -1 && j > 0 && j < game[0].length){
+                     // if(){}
+                    //}
 
                 }
+                counter++;
 
             }
         }
+
+        tailArray[2] = counter;
+        return tailArray;
     }
 
 
